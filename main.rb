96322aaecf5226a00d45ec7602bf6d296dec6812
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-08-29 14:45:19 -0400

    prints numbered and blank board

--- main.rb
+++ main.rb
@@ -1,16 +1,29 @@
-def new_board(title)#{}
+def board(title, board_array)
+	
 	puts "\n" + title +
 	"""
 	
-	   |   |   
-	___|___|___
-	   |   |   
-	___|___|___
-	   |   |   
-	   |   |
-	   
+   |   | 
+   |   |
+ #{board_array[0]} | #{board_array[1]} | #{board_array[2]}
+___|___|___
+   |   |   
+ #{board_array[3]} | #{board_array[4]} | #{board_array[5]} 
+   |   |   
+___|___|___
+   |   |
+ #{board_array[6]} | #{board_array[7]} | #{board_array[8]}
+___|___|___
+   |   |
+   |   |
+	  
 	   """
  end
+ 
+ board_positions_array = ["0","1","2","3","4","5","6","7","8"]
+ current_board_array = Array.new(9," ")
+ board("Welcome to Tic Tac Toe!" , board_positions_array)
+ board("The current board is", current_board_array)
  
 
 def player_1_marker
@@ -26,11 +39,15 @@ def player_2(marker)
 end
 
 def player_markers
-	player_1 = player_1_marker
-	player_2 = player_2(player_1)
-	puts "Player 1 is #{player_1} and Player 2 is #{player_2}."
+	@player_1 = player_1_marker
+	@player_2 = player_2(@player_1)
+	puts "Player 1 is #{@player_1} and Player 2 is #{@player_2}."
 end
 
-new_board("Would you like to play a game?")
-new_board("The current board is:")
-player_markers
+ # new_board("Would you like to play a game?")
+ # new_board("The current board is:")
+ # player_markers
+
+ # puts "Player 1, please choose a square, from 1-9"
+ # gets.to_i
+
