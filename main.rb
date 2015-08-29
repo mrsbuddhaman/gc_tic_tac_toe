aa1266f0e991a7792a53301488a0446f527b2df4
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-08-29 12:00:01 -0400

    define player markers

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-def new_board(title)
+def new_board(title)#{}
 	puts "\n" + title +
 	"""
 	
@@ -12,5 +12,25 @@ def new_board(title)
 	   """
  end
  
+
+def player_1_marker
+	puts "Player 1, please choose an X or O."
+	gets.chomp.upcase
+end
+
+def player_2(marker)
+	if marker == "X"
+		"O"
+	else "X"
+	end
+end
+
+def player_markers
+	player_1 = player_1_marker
+	player_2 = player_2(player_1)
+	puts "Player 1 is #{player_1} and Player 2 is #{player_2}."
+end
+
 new_board("Would you like to play a game?")
 new_board("The current board is:")
+player_markers
