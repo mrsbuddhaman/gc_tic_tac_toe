63471b779226cde1fbeef95b0043b6eccddf3eaf
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-08-29 10:17:58 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,15 @@
+def new_board(title)
+	puts "\n" + title +
+	"""
+	
+	   |   |   
+	___|___|___
+	   |   |   
+	___|___|___
+	   |   |   
+	   |   |
+	   
+	   """
+ end
+ 
+new_board("Would you like to play a game?")
