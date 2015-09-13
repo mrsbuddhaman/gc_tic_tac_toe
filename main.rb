8237ee0e1a78c1b1932b7b4f453852aa41d75829
mrsbuddhaman <mrs.buddhaman@gmail.com>
2015-09-13 12:47:53 -0400

    added tests for valid choices and full board and updated game code

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-
+class Game
 def board(title, board_array)
 	
 	puts "\n" + title +
@@ -22,15 +22,18 @@ ___|___|___
  end
 
  
- board_positions_array = ["1","2","3","4","5","6","7","8","9"]
- current_board_array = Array.new(9," ")
-# board("Welcome to Tic Tac Toe!" , board_positions_array)
-# board("The current board is", current_board_array)
+ 
+ 
  
 
 def player_1_marker
 	puts "Player 1, please choose an X or O."
-	gets.chomp.upcase
+	choice = gets.chomp.upcase
+	if marker_valid?(choice) == true
+	choice
+	else
+	player_1_marker
+	end
 end
 
 def player_2(marker)
@@ -50,18 +53,66 @@ def player_markers
 	@player_1 = player_1_marker
 	@player_2 = player_2(@player_1)
 	puts "Player 1 is #{@player_1} and Player 2 is #{@player_2}."
+	@current_player = @player_1
 end
 
-def square_valid?(square)
-	square >0 && square <10
+def square_valid?(square, board)
+	square >0 && square <10 && board[square-1] == (" ")
 end
-
- 
-  #player_markers
-
- # puts "Player 1, (#{@player_1})- please choose a square between 1 and 9"
-  #move = gets.to_i
-  
- # current_board_array[move - 1] = "#{@player_1}"
- # board("Player 1 (#{@player_1}) chose space #{move}", current_board_array)
-
+	def start_game
+		@board_positions_array = ["1","2","3","4","5","6","7","8","9"]
+		@current_board_array = Array.new(9," ")
+		board("Welcome to Tic Tac Toe!" ,@board_positions_array)
+		board("The current board is", @current_board_array)
+		player_markers
+		game_play
+	end
+	
+	def square_chooser	
+		if @current_player == @player_1
+			@n = 1
+		else
+			@n = 2
+		end
+		puts "Player #{@n}(#{@current_player})- please choose a square between 1 and 9"
+		@move = gets.to_i
+		if square_valid?(@move, @current_board_array) == true 
+		@move
+		else
+		square_chooser
+		end
+	end
+		
+	def update_board
+		@current_board_array[@move - 1] = "#{@current_player}"
+		board("Player #{@n} (#{@current_player}) chose space #{@move}", @current_board_array)
+	end
+	
+	def game_play
+		until board_full?(@current_board_array) == true
+		square_chooser
+		update_board
+		change_players
+		end
+	puts "Game Over"
+		
+	end
+	
+	def change_players
+		if @current_player == @player_1
+		   @current_player = @player_2
+		else
+		   @current_player = @player_1
+		 end
+	end
+	
+	def board_full?(status)
+		if status.include?(" ")
+			false
+		else
+			true
+		end
+	end
+	
+end
+Game.new.start_game
--- main_test.rb
+++ main_test.rb
@@ -3,24 +3,28 @@ require "minitest/autorun"
 
 class Main_test < Minitest::Test
 	def test_for_player_2
-		assert_equal("X",player_2("O"))	
-		assert_equal("O",player_2("X"))
+		assert_equal("X",Game.new.player_2("O"))	
+		assert_equal("O",Game.new.player_2("X"))
 	end
 	
 	def test_for_valid_player_1_marker
-		assert_equal(true, marker_valid?("X"))
-		assert_equal(true, marker_valid?("O"))
-		assert_equal(false, marker_valid?("A"))
+		assert_equal(true,Game.new.marker_valid?("X"))
+		assert_equal(true,Game.new.marker_valid?("O"))
+		assert_equal(false,Game.new.marker_valid?("A"))
 	end
 	
 	def test_for_valid_square_choose
-		assert_equal(true, square_valid?(3))
-		assert_equal(false, square_valid?(12))
-		assert_equal(false, square_valid?(0))
+		assert_equal(true, Game.new.square_valid?(3, [" "," "," "," "," ","X","O","X"," "]))
+		assert_equal(false, Game.new.square_valid?(12,[" "," "," "," "," ","X","O","X"," "]))
+		assert_equal(false, Game.new.square_valid?(0,[" "," "," "," "," ","X","O","X"," "] ))
+		assert_equal(false, Game.new.square_valid?(3,[" "," ","O"," "," ","X","O","X"," "] ))
+	end
+	
+	def test_for_full_board
+		assert_equal(true, Game.new.board_full?(["X","X","O","O","X","O","O","X","X"]))
+		assert_equal(false, Game.new.board_full?([" ","X","O","O","X","O","O","X","X"]))
 	end
 		
-
-
 
 
 
